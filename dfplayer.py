43604ea9cc3eedc23e6a2b0d22edcd21d495a01e
Cefn Hoile <github.com@cefn.com>
2017-07-10 15:13:00 +0100

    Renamed Dfplayer to Player. Added config, play and volume delay logic. Exposed various extra untested hex commands (repeat, standby, wake, reset). Added exposure of volume adjust as 'gain'.

--- dfplayer.py
+++ dfplayer.py
@@ -1,65 +1,156 @@
-from machine import UART,Pin
-from time import sleep
+from machine import UART, Pin
+from utime import sleep_ms, ticks_ms, ticks_diff
 
-Start_Byte=0x7E
-Version_Byte=0xFF
-Command_Length=0x06
-Acknowledge=0x00
-End_Byte=0xEF
+Start_Byte = 0x7E
+Version_Byte = 0xFF
+Command_Length = 0x06
+Acknowledge = 0x00
+End_Byte = 0xEF
+
+# inherent delays in DFPlayer
+CONFIG_LATENCY = 500
+PLAY_LATENCY =   500
+VOLUME_LATENCY = 500
+
+def clamp(x, minimum, maximum):
+    return max(minimum, min(x, maximum))
 
 def split(num):
     return num >> 8, num & 0xFF
 
-class Dfplayer():
-    def __init__(self, uart=None, busy_pin=None, config=True):
+def kill_time(stamp_ms, kill_ms):
+    diff_ms = ticks_diff(ticks_ms(), stamp_ms)
+    if diff_ms < kill_ms:
+        snooze_ms = kill_ms - diff_ms
+        sleep_ms(snooze_ms)
+        return snooze_ms
+    else:
+        return 0
+
+class Player():
+    def __init__(self, uart=None, busy_pin=None, config=True, volume=0.5):
+        self._volume = None
         if uart is None:
             self.uart = UART(1, 9600) # UART on 
             self.uart.init(9600, bits=8, parity=None, stop=1)
         else:
             self.uart = uart
         if busy_pin is not None:
-            busy_pin.init(mode=Pin.IN)
+            busy_pin.init(mode=Pin.IN, pull=Pin.PULL_UP)
         self.busy_pin = busy_pin
         if config:
             self.config()
+        if volume is not None:
+            self.volume(volume)
 
-    def config(self):
-        self.execute(0x3F, 0x00, 0x00)
-
-    def execute(self, CMD, Par1, Par2):
+    def command(self, CMD, Par1, Par2):
+        self.awaitconfig()
         Checksum = -(Version_Byte + Command_Length + CMD + Acknowledge + Par1 + Par2)
         HighByte, LowByte = split(Checksum)
-        CommandLine = bytes([b & 0xFF for b in [Start_Byte, Version_Byte, Command_Length, CMD, Acknowledge,
+        CommandLine = bytes([b & 0xFF for b in [
+            Start_Byte, Version_Byte, Command_Length, CMD, Acknowledge,
             Par1, Par2, HighByte, LowByte, End_Byte
         ]])
         self.uart.write(CommandLine)
 
-    def play(self, dirNum, fileNum):
-        self.execute(0x0F,dirNum,fileNum)
+    def config(self):
+        self.configtime = ticks_ms()
+        #self.reset()
+        self.command(0x3F, 0x00, 0x00)
 
-    def volume(self, val=30):
-        self.execute(0x06,0x00,val)  
+    def play(self, folderNum, fileNum):
+        self.awaitconfig()
+        self.playtime = ticks_ms()
+        self.command(0x0F,folderNum,fileNum)
 
     def playing(self):
-        return self.busy_pin.value() == 0
+        if self.busy_pin is not None:
+            self.awaitplay()
+            return self.busy_pin.value() == 0
+        else:
+            raise AssertionError("No busy pin provided, cannot detect play status")
+
+    def awaitconfig(self):
+        if self.configtime is not None:
+            kill_time(self.configtime, CONFIG_LATENCY)
+        self.configtime = None
+
+    def awaitplay(self):
+        if self.playtime is not None: # handle delay between playing and registering
+            kill_time(self.playtime, PLAY_LATENCY)
+        self.playtime = None
+
+    def awaitvolume(self):
+        if self.volumetime is not None: # handle delay between playing and registering
+            kill_time(self.volumetime, VOLUME_LATENCY)
+        self.volumetime = None
+
+    def repeat(self, repeat=True):
+        self.awaitconfig()
+        val = 1 if repeat else 0
+        self.command(0x11, 0, val)
+
+    def _gain(self, gain=1.0):
+        self.awaitconfig()
+        gain = float(clamp(gain, 0, 1.0))
+        val = int(30.0 * gain)
+        self.command(0x10,0 ,val)  
+
+    def volume(self, volume=None):
+        self.awaitconfig()
+        if volume is None:
+            return self._volume
+        else:
+            self._volume = float(clamp(volume, 0, 1.0))
+            val = int(30.0 * self._volume)
+            self.command(0x06,0 ,val)
+            self.volumetime = ticks_ms()
+
+    def standby(self):
+        self.awaitconfig()
+        self.command(0x0A, 0x00, 0x00)
+
+    def wake(self):
+        self.awaitconfig()
+        self.command(0x0B, 0x00, 0x00)
+
+    def reset(self):
+        self.awaitconfig()
+        self.command(0x0C, 0x00, 0x00)
 
 def main():
     from machine import Pin
     from time import sleep
     from dfplayer import Dfplayer
-    player = Dfplayer(busy_pin=Pin(0), config=False)
-    player.config()
-    sleep(0.1)
-    #player.volume(30)
+    player = Dfplayer(busy_pin=Pin(0))
+    #player.volume(0.5)
     folder = 1
     while True:
         for track in range(2, 7):
             print("Playing track {}".format(track))
             player.play(folder,track)
-            sleep(0.2)
             while player.playing():
-                sleep(0.05)
+                sleep(0.01)
+
+"""
+from machine import Pin
+from time import sleep
+from dfplayer import *
+player = Player(busy_pin=Pin(0))
+player.volume(0.1)
+tracks = {}
+for folderNum in range(0,10):
+    for fileNum in range(0,256):
+        player.play(folderNum,fileNum)
+        if player.playing():
+            if not(folderNum in tracks):
+                tracks[folderNum] = []
+            tracks[folderNum].append(fileNum)
+            continue
+        else:
+            break
+print(tracks)
+player.volume(0.5)
+"""
 
 
-def scan():
-    player = Dfplayer(busy_pin=Pin(0), config=False)
